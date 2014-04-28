d87169c0c94a1c1e628cf0f6477fb2c8b069ca50
chendingding <chendingding@jike.com>
2014-04-28 17:03:37 +0800

    change: init

---
+++ Classes/HomeScene.cpp
@@ -0,0 +1,57 @@
+#include "HomeScene.h"
+
+USING_NS_CC;
+
+Scene* HomeScene::createScene()
+{
+    // 'scene' is an autorelease object
+    auto scene = Scene::create();
+    
+    // 'layer' is an autorelease object
+    auto layer = HomeScene::create();
+
+    // add layer as a child to scene
+    scene->addChild(layer);
+
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool HomeScene::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+    
+    Size visibleSize = Director::getInstance()->getVisibleSize();
+    Point origin = Director::getInstance()->getVisibleOrigin();
+    
+    auto label = LabelTTF::create("Start", "Arial", 32);
+    
+    label->setPosition(Point(origin.x + visibleSize.width/2,
+                             origin.y + visibleSize.height - label->getContentSize().height));
+    
+    this->addChild(label, 1);
+    
+    
+    return true;
+}
+
+
+void HomeScene::menuCloseCallback(Ref* pSender)
+{
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WP8) || (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT)
+	MessageBox("You pressed the close button. Windows Store Apps do not implement a close button.","Alert");
+    return;
+#endif
+
+    Director::getInstance()->end();
+
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+    exit(0);
+#endif
+}
